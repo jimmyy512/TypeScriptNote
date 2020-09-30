82e67e9ff8cacb7886eafb2d1fa34b30b84ed436
jim <jim.zhuang@arionlabs.com.tw>
2020-09-30 11:57:14 +0800

    add mobx

---
+++ common/2.Decorator.ts
@@ -0,0 +1,34 @@
+function classDecorator(target:any){
+    console.log("---classDecorator:");
+    console.log(new target("cool"));
+}
+
+function methodDecorator(target:any,name:string,descriptor:PropertyDescriptor){
+    console.log("---methodDecorator:");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function propertyDecorator(target:any,propertyKey: string){
+    console.log("---propertyDecorator:");
+    console.log(target,propertyKey);
+}
+
+
+@classDecorator
+class Blog{
+    
+    @propertyDecorator author:string;
+
+    blogs:any[]=[];
+    
+    constructor(author:string) {
+        this.author=author;
+    }
+
+    @methodDecorator
+    getNum(){
+        return this.blogs.length;
+    }
+}
