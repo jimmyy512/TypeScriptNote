6bb97bb4e3a1fe185e94629ef105b3b785f3d53c
jim <jim.zhuang@arionlabs.com.tw>
2020-09-30 13:17:09 +0800

    use Decorator

---
+++ mobx/2.useDecorator.ts
@@ -0,0 +1,25 @@
+import {observable, computed,autorun} from "mobx";
+
+class OrderLine {
+    @observable price = 0;
+    @observable amount = 1;
+
+    constructor(price:number) {
+        this.price = price;
+    }
+
+    @computed get total() {
+        console.log("computed total call:",this.price,this.amount);
+        return this.price * this.amount;
+    }
+}
+
+const orderLine=new OrderLine(100);
+autorun(()=>{
+    console.log("autorun total:",orderLine.total);
+})
+
+
+setInterval(()=>{
+    orderLine.price=Math.random()*100;
+},1000)
