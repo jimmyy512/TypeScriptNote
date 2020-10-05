f2db50ec2852e162f4847c1ec9c70180ad63cb78
jim <jim.zhuang@arionlabs.com.tw>
2020-10-05 11:44:26 +0800

    add action.js

---
+++ mobx/4.action.ts
@@ -0,0 +1,20 @@
+import { observable, computed, autorun, action, when } from "mobx";
+class Ticker {
+    @observable tick = 0
+
+    @action.bound
+    increment() {
+        this.tick++ // 'this' 永远都是正确的
+        console.log(this.tick,this);
+    }
+
+    constructor() {
+        autorun(() =>{
+            console.log(this.tick);
+        })
+    }
+
+}
+
+const ticker = new Ticker()
+setInterval(ticker.increment, 1000)
