88e9e4c80dbf6f5e0feef7c510d0b761ef4b3160
Kano.c <jimmyy5121@gmail.com>
2022-01-07 16:11:43 +0800

    修改 lintstaged規則

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-console.log("test")
+console.log('test');
 
-let test = 123;
-console.log("123")
-function testFunc( num) {
-  return num+10
+const test = 123;
+console.log('123');
+function testFunc(num: any) {
+  return num + 10;
 }
-let res = testFunc(100);
-console.log("res:",res);
+const res = testFunc(100);
+console.log('res:', res);
